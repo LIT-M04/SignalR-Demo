fb3963edc6b41cb5db804272b1e15741997e6eeb
Avrumi Friedman <info@lakewoodprogramming.com>
2016-07-07 11:34:58 -0400

    Initial commit

---
+++ MvcApplication36/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.SignalR;
+
+namespace MvcApplication36.Controllers
+{
+
+
+    public class HomeController : Controller
+    {
+        //static HomeController()
+        //{
+        //    Random rnd = new Random();
+        //    var th = new Thread(w =>
+        //    {
+        //        while (true)
+        //        {
+        //            Thread.Sleep(rnd.Next(500, 1000));
+        //            var context = GlobalHost.ConnectionManager.GetHubContext<SimpleHub>();
+        //            context.Clients.All.newMessageReceived(rnd.Next(1, 1928682736));
+        //        }
+        //    });
+        //    th.Start();
+        //}
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Message(string message)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<SimpleHub>();
+            context.Clients.All.newMessageReceived(new { time = DateTime.Now.ToLongTimeString(), message = message });
+        }
+    }
+}
---
+++ MvcApplication36/Scripts/index.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    var simpleHub = $.connection.simpleHub;
+    $.connection.hub.start();
+
+    simpleHub.client.newMessageReceived = function(result) {
+        $("ul").append("<li>" + result.message + " at: " + result.time + "</li>");
+    }
+
+    $('.btn').on('click', function() {
+        $.post("/home/message", { message: $("#message").val() });
+        $("#message").val('');
+    });
+
+});
